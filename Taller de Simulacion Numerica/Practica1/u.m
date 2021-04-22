e8a668bdb2c3ef664bcaf97ec7f8e68c76239e2f
Helus <43776278+Helus@users.noreply.github.com>
2021-04-22 10:49:49 +0200

    Delete Taller de Simulacion Numerica/Practica1 directory

--- Taller de Simulacion Numerica/Practica1/u.m
+++
@@ -1,4 +0,0 @@
-function z=u(t)
-z=2*exp(t);
-% Observar que la variable de salida z tiene la misma dimensión que el
-% argumento de entrada t
--- Taller de Simulacion Numerica/Practica1/v.m
+++
@@ -1,4 +0,0 @@
-function z=v(t)
-z=-1*ones(1,length(t));
-%Puesto que el parámetro de discretización es un vector fila, se considera
-%como variable de salida un vector de la misma dimensión.
