a4805d781daa8047c05b57c53c961aef39466347
Helus <43776278+Helus@users.noreply.github.com>
2021-11-13 21:11:01 +0100

    Delete M2I/Opt_Control directory

--- M2I/Opt_Control/1/f.m
+++
@@ -1,9 +0,0 @@
-% f: función a minimizar f(x)=5*sum_{i=1}^n i*x(i)^2
-
-function z=f(x)
-global xx;
-n=length(x);
-z=5*dot(x.^2,[1:n]);
-
-xx=[xx; x];
-end
--- M2I/Opt_Control/2/pruebas/f_ex1c.m
+++
@@ -1,4 +0,0 @@
-% Function to be minimized in Example 1
-  function f=f_ex1c(v)
-  
-  f = 3*v(1)^2+2*v(2)^2-v(1)-2*v(2);
