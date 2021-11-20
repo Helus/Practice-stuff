a8ca7a9844fc7dd1a7d451c4c95bdbf8f89b93c8
Helus <43776278+Helus@users.noreply.github.com>
2021-11-20 12:47:34 +0100

    Delete M2I/Opt_Control/2 directory

--- M2I/Opt_Control/2/test_DIY2.m
+++
@@ -1,6 +0,0 @@
-% test_DIY2: función que comprueba si un punto es factible en el problema DIY2
-
-function logical=test_DIY2(v)
-A=[-3 -2; -1 1; 1 1; 2/3 -1]; b=[6 -3 -7 -4/3]';
-logical=A*v+b<=1.e-8;    % tol=10^(-8)
-end
