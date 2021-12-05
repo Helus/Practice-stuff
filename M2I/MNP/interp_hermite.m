68a6ad9cca62fbe6db24881695424dd36fe44549
Helus <43776278+Helus@users.noreply.github.com>
2021-12-05 21:46:30 +0100

    Add files via upload

--- M2I/MNP/interp_hermite.m
+++ M2I/MNP/interp_hermite.m
@@ -16,14 +16,12 @@ dif(end)=x_interp-x(end);
 
 H=[1];
 for i=1:(2*N-1)
-    juan=[ones(1,i),zeros(1,2*N-1-i)];
-    vprod=juan.*dif;
-    j=1; prod=1;
-    while j<length(vprod) && abs(vprod(j))>tol
-        prod=prod*vprod(j); j=j+1;
+    prod=1;
+    for j=1:i
+        prod=prod*dif(j);
     end
+    
     H=[H prod];
 end
-
 H=sum(Q.*H);
 fprintf('valor de f(%4.3f)=%f\n',x_interp,H)
