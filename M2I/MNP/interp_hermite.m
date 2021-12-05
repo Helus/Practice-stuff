edf6047baa3d24256c3996da5756c8b741d3ed71
Helus <43776278+Helus@users.noreply.github.com>
2021-12-05 21:20:33 +0100

    Add files via upload

--- M2I/MNP/interp_hermite.m
+++ M2I/MNP/interp_hermite.m
@@ -2,9 +2,11 @@ clear all
 close all
 
 datos
-Q=hermite(x,fx,dfx);
-tol=1.e-10;
 N=length(x);
+tol=1.e-10;
+
+Q=hermite(x,fx,dfx);
+
 dif=zeros(1,2*N-1);
 for i=1:N-1
     dif(2*i-1)=x_interp-x(i);
